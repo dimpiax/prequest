ee2c2b9157e825f37342300eae2f196716e2920c
Pilipenko Dima <dimpiax@gmail.com>
2017-03-06 02:31:43 +0200

    added __esModule support

--- index.js
+++ index.js
@@ -10,27 +10,14 @@ const promiseWrapper = (req) =>
         });
     });
 
-const prequest = promiseWrapper(request)
+const prequest = promiseWrapper(request);
 
-module.exports = {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+Object.assign(exports, {
     default: prequest,
     request,
     promiseWrapper
-}
-
-// const promiseWrapper = (r: request): ((data: any) => Promise<Object>) =>
-//     (data: any): Promise<Object> => new Promise((resolve: any, reject: any) => {
-//         r(data, (error: any, response: any, body: any) => {
-//             if (error) reject(error)
-//             else resolve(body)
-//         })
-//     })
-//
-//
-// const prequest = promiseWrapper(request)
-// export default prequest
-//
-// export {
-//     request,
-//     promiseWrapper
-// }
+});
